e0af90e67e878abec9c72dfcb073661abc607129
Cemre Fatih Karakulak <cradexco@gmail.com>
2021-05-11 20:51:29 +0300

    reverting quote.js

--- examples/quotes.js
+++ examples/quotes.js
@@ -23,3 +23,38 @@ ten99policy.quotes.create({
 })
   .then(message => console.log(message))
   .catch(error => console.error(error));
+
+/*-----------------------------------------------------------------------------------*/
+/* Updating a quote (replace xxx with an existing quote id)
+/*-----------------------------------------------------------------------------------*/
+
+ten99policy.quotes.update('xxx', {
+  coverage_type: "general",
+  effective_date: "2019-07-11"
+})
+  .then(message => console.log(message))
+  .catch(error => console.error(error));
+
+/*-----------------------------------------------------------------------------------*/
+/* Fetching the list of quotes
+/*-----------------------------------------------------------------------------------*/
+
+ten99policy.quotes.list()
+  .then(message => console.log(message))
+  .catch(error => console.error(error));
+
+/*-----------------------------------------------------------------------------------*/
+/* Retrieving a quote (replace xxx with an existing quote id)
+/*-----------------------------------------------------------------------------------*/
+
+ten99policy.quotes.retrieve('xxx')
+  .then(message => console.log(message))
+  .catch(error => console.error(error));
+
+/*-----------------------------------------------------------------------------------*/
+/* Delete a quote (replace xxx with an existing quote id)
+/*-----------------------------------------------------------------------------------*/
+
+ten99policy.quotes.del('xxx')
+  .then(message => console.log(message))
+  .catch(error => console.error(error));
