0b0752db9b86385723d55323f83ec7d3df312a4b
Cemre Fatih Karakulak <cradexco@gmail.com>
2021-05-11 21:11:01 +0300

    first test: Config must be an object

---
+++ testUtils/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('mocha');
+require('chai').use(require('chai-as-promised'));
+
+const utils = (module.exports = {
+  getUserTen99PolicyKey: () => {
+    const key =
+      process.env.TEN99POLICY_TEST_API_KEY || 't9sk_test_29374df8-4462-4900-ad3a-145aa46fbfca';
+
+    return key;
+  },
+});
